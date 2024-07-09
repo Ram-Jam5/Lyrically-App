2d2dc1794ec58429139cf84179ca86ae93d277ec
Ram-Jam5 <edgeillusive@gmail.com>
2024-07-09 09:11:19 -0500

    build the lyrics index page

--- controllers/lyrics.js
+++ controllers/lyrics.js
@@ -2,5 +2,13 @@ const express= require('express');
 const router = express.Router();
 
 const User = require('../models/user.js')
+router.get('/', (req, res) => {
+    try {
+        res.render('lyrics/index.ejs')
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+})
 
 module.exports = router;
